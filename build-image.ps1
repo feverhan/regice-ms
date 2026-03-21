28c620da7935fc42780c1d1e2d7336eab60f6d12
hanqw <hanqw@ztt.cn>
2026-03-21 08:22:20 +0800

    ``` feat(build): update image name to use Aliyun container registry
    - Change default image name from "regice-ms:latest" to
      "crpi-7ove6igc635triub.cn-shanghai.personal.cr.aliyuncs.com/hanqw_ztt/regice_ms:latest"
      in both build-image.ps1 and build-image-podman.ps1 scripts
    - Update container name in docker-compose.yml to match the new registry path
    - This enables deployment to Aliyun Container Registry instead of local images
    ```

--- build-image.ps1
+++ build-image.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$ImageName = if ($args.Length -gt 0 -and $args[0]) { $args[0] } else { "regice-ms:latest" }
+$ImageName = if ($args.Length -gt 0 -and $args[0]) { $args[0] } else { "crpi-7ove6igc635triub.cn-shanghai.personal.cr.aliyuncs.com/hanqw_ztt/regice_ms:latest" }
 
 Write-Host "Building Docker image: $ImageName"
 docker build -t $ImageName .
